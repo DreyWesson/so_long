df7418982fe35952994f40f1a5ffa51891184a96
dreywesson <dreywesson@gmail.com>
2023-08-17 05:26:38 +0100

    refactored handle-N-S-E-W funcs

--- src/map/index.c
+++ src/map/index.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 16:27:09 by doduwole          #+#    #+#             */
-/*   Updated: 2023/08/17 04:56:02 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/08/17 05:25:50 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,81 +90,98 @@ t_cell	**create_grid(char **map, t_details *details)
 	return (grid);
 }
 
-void handle_north(t_cell **grid, t_nodes **queue, t_cell *curr_node)
+void	handle_north(t_cell **grid, t_nodes **queue, t_details *details)
 {
+	int x;
+	int y;
+
+	x = details->pos.x;
+	y = details->pos.y;
 	if (
-		curr_node->y_axis - 1 >= 0 
-			&& grid[curr_node->y_axis - 1][curr_node->x_axis].status == RESTING
-			&& grid[curr_node->y_axis - 1][curr_node->x_axis].val != WALL
-		) // NORTH
+		(y - 1) >= 0
+		&& grid[y - 1][x].status == RESTING
+		&& grid[y - 1][x].val != WALL
+		)
 	{
-		add_head_node(queue, create_node(&grid[curr_node->y_axis - 1][curr_node->x_axis]));
+		add_head_node(queue, create_node(&grid[y - 1][x]));
 	}
 }
 
-void handle_south(t_cell **grid, t_nodes **queue, t_cell *curr_node, t_details *details)
+void	handle_south(t_cell **grid, t_nodes **queue, t_details *details)
 {
+	int x;
+	int y;
+	int row_nbr;
+
+	x = details->pos.x;
+	y = details->pos.y;
+	row_nbr = details->row_nbr;
 	if (
-		curr_node->y_axis + 1 <= details->row_nbr 
-			&& grid[curr_node->y_axis + 1][curr_node->x_axis].status == RESTING
-			&& grid[curr_node->y_axis + 1][curr_node->x_axis].val != WALL
-		) // SOUTH
+		(y + 1) <= row_nbr 
+		&& grid[y + 1][x].status == RESTING
+		&& grid[y + 1][x].val != WALL
+		)
 	{
-		add_head_node(queue, create_node(&grid[curr_node->y_axis + 1][curr_node->x_axis]));
-		grid[curr_node->y_axis + 1][curr_node->x_axis].status = WAITING;
+		add_head_node(queue, create_node(&grid[y + 1][x]));
+		grid[y + 1][x].status = WAITING;
 	}
 }
-void handle_west(t_cell **grid, t_nodes **queue, t_cell *curr_node)
+void	handle_west(t_cell **grid, t_nodes **queue, t_details *details)
 {
+	int x;
+	int y;
+
+	x = details->pos.x;
+	y = details->pos.y;
 	if (
-		curr_node->x_axis - 1 >= 0
-			&& grid[curr_node->y_axis][curr_node->x_axis - 1].status == RESTING
-			&& grid[curr_node->y_axis][curr_node->x_axis - 1].val != WALL
-		) // WEST
+		(x - 1) >= 0
+		&& grid[y][x - 1].status == RESTING
+		&& grid[y][x - 1].val != WALL
+		)
 	{
-		add_head_node(queue, create_node(&grid[curr_node->y_axis][curr_node->x_axis - 1]));
-		grid[curr_node->y_axis][curr_node->x_axis - 1].status = WAITING;
+		add_head_node(queue, create_node(&grid[y][x - 1]));
+		grid[y][x - 1].status = WAITING;
 	}
 }
 
-void handle_east(t_cell **grid, t_nodes **queue, t_cell *curr_node, t_details *details)
+void	handle_east(t_cell **grid, t_nodes **queue, t_details *details)
 {
 	int x;
 	int y;
 	int col_nbr;
 
-	x = curr_node->x_axis;
-	y = curr_node->y_axis;
+	x = details->pos.x;
+	y = details->pos.y;
 	col_nbr = details->col_nbr;
 	if ((x + 1) <= col_nbr
-			&& grid[y][x + 1].status == RESTING
-			&& grid[y][x + 1].val != WALL
-		) // EAST
+		&& grid[y][x + 1].status == RESTING
+		&& grid[y][x + 1].val != WALL
+		)
 	{
 		add_head_node(queue, create_node(&grid[y][x + 1]));
 		grid[y][x + 1].status = WAITING;
 	}
 }
 
-	// (void)size;
-	// check if it's not been visited or waiting
-	// add to queue
-	// add its adjacent nodes to queue
-	// pop the visited from queue
-	// call with next on queue
-	// if its equal to resting && not a wall
-	// del_node(queue);
+// (void)size;
+// check if it's not been visited or waiting
+// add to queue
+// add its adjacent nodes to queue
+// pop the visited from queue
+// call with next on queue
+// if its equal to resting && not a wall
+// del_node(queue);
 void bfs(t_cell **grid,t_nodes **queue, t_cell *curr_node, t_details *details)
 {
-	// t_coord node_details;
 	if (!*queue)
 		return ;
-	handle_north(grid, queue, curr_node);
-	handle_south(grid, queue, curr_node, details);
-	handle_east(grid, queue, curr_node, details);
-	handle_west(grid, queue, curr_node);
+	details->pos.x = curr_node->x_axis;
+	details->pos.y = curr_node->y_axis;
+	handle_north(grid, queue, details);
+	handle_south(grid, queue, details);
+	handle_east(grid, queue, details);
+	handle_west(grid, queue, details);
 	curr_node->status = VISITED;
-	(void)details;
 }
 
 void	handle_map(char **argv)
