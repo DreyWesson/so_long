20a66df72912d32b85a825625c57430470abb9c2
dreywesson <dreywesson@gmail.com>
2023-08-18 10:37:28 +0100

    handle_directions function refactored

--- src/map/helper/two.c
+++ src/map/helper/two.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 06:28:34 by doduwole          #+#    #+#             */
-/*   Updated: 2023/08/18 06:29:52 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/08/18 10:36:19 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,31 +34,24 @@ t_cell	**create_grid(char **map, t_details *details)
 	return (grid);
 }
 
+void	direction_math(t_cell **grid, t_nodes **queue, int y, int x)
+{
+	if (grid[y][x].status == SPACE && grid[y][x].val != WALL)
+	{
+		add_head_node(queue, create_node(&grid[y][x]));
+		grid[y][x].status = WAITING;
+	}
+}
+
 void	handle_directions(t_cell **grid, t_nodes **queue, t_details *details)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	x = details->pos.x;
 	y = details->pos.y;
-	if (grid[y - 1][x].status == SPACE && grid[y - 1][x].val != WALL)
-	{
-		add_head_node(queue, create_node(&grid[y - 1][x]));
-		grid[y - 1][x].status = WAITING;
-	}
-	if (grid[y + 1][x].status == SPACE && grid[y + 1][x].val != WALL)
-	{
-		add_head_node(queue, create_node(&grid[y + 1][x]));
-		grid[y + 1][x].status = WAITING;
-	}
-	if (grid[y][x - 1].status == SPACE && grid[y][x - 1].val != WALL)
-	{
-		add_head_node(queue, create_node(&grid[y][x - 1]));
-		grid[y][x - 1].status = WAITING;
-	}
-	if (grid[y][x + 1].status == SPACE && grid[y][x + 1].val != WALL)
-	{
-		add_head_node(queue, create_node(&grid[y][x + 1]));
-		grid[y][x + 1].status = WAITING;
-	}
+	direction_math(grid, queue, y - 1, x);
+	direction_math(grid, queue, y + 1, x);
+	direction_math(grid, queue, y, x - 1);
+	direction_math(grid, queue, y, x + 1);
 }
