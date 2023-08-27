31808cc5aa0fc065e55b018d1efbedb8bf6a37d7
dreywesson <dreywesson@gmail.com>
2023-08-27 15:00:12 +0100

    open xpm file errors implemented

--- src/images/index.c
+++ src/images/index.c
@@ -6,12 +6,46 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 09:08:40 by doduwole          #+#    #+#             */
-/*   Updated: 2023/08/27 12:18:50 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/08/27 14:59:29 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
 
+void	door_imgs_error(t_game game)
+{
+	if (!game.door_close_img)
+		printf("door_01.xpm file not opening\n");
+	if (!game.door_open_img)
+		printf("door_02.xpm file not opening\n");
+	if (!game.door_close_img || !game.door_open_img)
+		exit(1);
+}
+
+void	collect_imgs_error(t_game game)
+{
+	if (!game.collects_imgs.img_0)
+		printf("plant_03.xpm file not opening\n");
+	if (!game.collects_imgs.img_1)
+		printf("plant_04.xpm file not opening\n");
+	if (!game.effect.img)
+		printf("effect_w.xpm file not opening\n");
+	if (!game.collects_imgs.img_0 || !game.collects_imgs.img_1 || !game.effect.img)
+		exit(1);
+}
+
+void	player_imgs_error(t_game game)
+{
+	if (!game.player.idle_img_0)
+		printf("player_01.xpm file not opening\n");
+	if (!game.player.idle_img_1)
+		printf("player_02.xpm file not opening\n");
+	if (!game.player.action_img)
+		printf("player_03.xpm file not opening\n");
+	if (!game.player.idle_img_0 || !game.player.idle_img_1
+		|| !game.player.action_img)
+		exit(1);
+}
 
 static void	open_door_imgs(t_game *game)
 {
@@ -21,6 +55,7 @@ static void	open_door_imgs(t_game *game)
 	game->door_close_img = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/door_02.xpm",
 			&game->img_size.x, &game->img_size.y);
+	door_imgs_error(*game);
 }
 
 static void	open_collect_imgs(t_game *game)
@@ -35,6 +70,7 @@ static void	open_collect_imgs(t_game *game)
 	game->effect.img = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/effect_w.xpm",
 			&game->img_size.x, &game->img_size.y);
+	collect_imgs_error(*game);
 }
 
 static void	open_player_imgs(t_game *game)
@@ -46,6 +82,7 @@ static void	open_player_imgs(t_game *game)
 	game->player.action_img = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/player_03.xpm", &game->img_size.x, &game->img_size.y);
 	game->player.current_img = game->player.action_img;
+	player_imgs_error(*game);
 }
 
 void	open_images(t_game *game)
--- src/images/two.c
+++ src/images/two.c
@@ -6,12 +6,42 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 07:33:36 by doduwole          #+#    #+#             */
-/*   Updated: 2023/08/27 12:23:09 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/08/27 14:59:24 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
 
+void	downwall_imgs_error(t_wall_img wall)
+{
+	if (!wall.down_right)
+		printf("wall_dr.xpm file not opening\n");
+	if (!wall.down)
+		printf("wall_d.xpm file not opening\n");
+	if (!wall.down_left)
+		printf("wall_dl.xpm file not opening\n");
+	if (!wall.left)
+		printf("wall_l.xpm file not opening\n");
+	if (!wall.down_right || !wall.down || !wall.down_left || !wall.left)
+		exit(1);
+}
+
+void	upwall_imgs_error(t_wall_img wall)
+{
+	if (!wall.block)
+		printf("wall_02.xpm file not opening\n");
+	if (!wall.up_left)
+		printf("wall_ul.xpm file not opening\n");
+	if (!wall.up)
+		printf("wall_u.xpm file not opening\n");
+	if (!wall.up_right)
+		printf("wall_ur.xpm file not opening\n");
+	if (!wall.right)
+		printf("wall_r.xpm file not opening\n");
+	if (!wall.block || !wall.up_left || !wall.up || !wall.up_right
+		|| !wall.right)
+		exit(1);
+}
 
 void	open_wallimgs_down(t_game *game)
 {
@@ -27,6 +57,7 @@ void	open_wallimgs_down(t_game *game)
 	game->wall_imgs.left = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/wall_l.xpm",
 			&game->img_size.x, &game->img_size.y);
+	downwall_imgs_error(game->wall_imgs);
 }
 
 void	open_wallimgs_up(t_game *game)
@@ -34,9 +65,6 @@ void	open_wallimgs_up(t_game *game)
 	game->wall_imgs.block = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/wall_02.xpm",
 			&game->img_size.x, &game->img_size.y);
-	printf("%d %d\n",game->img_size.x, game->img_size.y);
-	if (!game->wall_imgs.block)
-		printf("debug\n");
 	game->wall_imgs.up_left = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/wall_ul.xpm",
 			&game->img_size.x, &game->img_size.y);
@@ -49,4 +77,5 @@ void	open_wallimgs_up(t_game *game)
 	game->wall_imgs.right = mlx_xpm_file_to_image(game->mlx,
 			"./sprites/wall_r.xpm",
 			&game->img_size.x, &game->img_size.y);
+	upwall_imgs_error(game->wall_imgs);
 }
