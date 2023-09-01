e6cf70a5cd7a66cfe02a7562489eadbe8c4dd3b5
dreywesson <dreywesson@gmail.com>
2023-09-01 16:26:05 +0100

    makefile libft fixed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 17:05:51 by doduwole          #+#    #+#             */
-/*   Updated: 2023/08/31 22:56:21 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/09/01 16:25:51 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	main(int argc, char **argv)
 {
 	t_game	game;
-
 
 	if (!handle_validation(argc, argv, &game))
 		exit(1);
